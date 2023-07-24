54c4d4c800b3dad9b29327c8f99cba8b075bc36f
lorenzoM1806 <lorenzomiechielsen@hotmail.com>
2023-07-24 15:56:43 +0200

    fire base config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   firebaseConfig: {
     apiKey: 'AIzaSyBWuAgrCAAa_dz1L-exMSquk9RRwK5zh-U',
-    authDomain: 'application-f499e.firebaseapp.com',
-    projectId: 'application-f499e',
-    storageBucket: 'application-f499e.appspot.com',
-    messagingSenderId: '93910067888',
-    appId: '1:93910067888:web:c209895bfd516a63d7e975',
-    measurementId: 'G-W4GEVKVH9B'
+  authDomain: 'application-f499e.firebaseapp.com',
+  projectId: 'application-f499e',
+  storageBucket: 'application-f499e.appspot.com',
+  messagingSenderId: '93910067888',
+  appId: '1:93910067888:web:c209895bfd516a63d7e975',
+  measurementId: 'G-W4GEVKVH9B'
   }
 };
